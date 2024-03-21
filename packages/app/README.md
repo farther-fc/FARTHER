07804a4cb97a67dfe5dc868cf5767b169d265363
Matt Masurka <m.masurka@gmail.com>
2024-03-20 21:57:50 -0700

    Adds NavMenu

--- packages/app/README.md
+++ packages/app/README.md
@@ -14,7 +14,6 @@ npx create-next-app -e https://github.com/shadcn/next-template
 - Radix UI Primitives
 - Tailwind CSS
 - Icons from [Lucide](https://lucide.dev)
-- Dark mode with `next-themes`
 - Tailwind CSS class sorting, merging and linting.
 
 ## License
--- packages/app/components/nav/SiteHeader.tsx
+++ packages/app/components/nav/SiteHeader.tsx
@@ -1,13 +1,33 @@
-import { ThemeToggle } from "@components/ThemeToggle";
-import { MainNav } from "@components/nav/MainNav";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount, useDisconnect } from "wagmi";
+import Link from "next/link";
+import { NavMenu } from "@components/nav/NavMenu";
 
 export function SiteHeader() {
+  const { open, close } = useWeb3Modal();
+  const { isDisconnected } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const connectButtonClick = () => {
+    if (isDisconnected) {
+      open();
+    } else {
+      close();
+      disconnect();
+    }
+  };
+
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center sm:justify-between">
-        <MainNav />
-        <div className="ml-4">
-          <ThemeToggle />
+        <Link href="/" className="flex items-center ">
+          F A R T H E R
+        </Link>
+        <div className="flex space-x-6">
+          <button className="btn-primary" onClick={connectButtonClick}>
+            {isDisconnected ? "Connect" : "Disconnect"}
+          </button>
+          <NavMenu />
         </div>
       </div>
     </header>
