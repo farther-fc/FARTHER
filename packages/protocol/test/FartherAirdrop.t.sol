856c0f17aff263e7813d25fa00c8964242302274
Matt Masurka <m.masurka@gmail.com>
2024-03-23 10:46:49 -0700

    Starting airdrop tests

--- packages/protocol/test/FartherAirdrop.t.sol
+++ packages/protocol/test/FartherAirdrop.t.sol
@@ -2,12 +2,27 @@
 pragma solidity 0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
+// import {Merkle} from "murky/Merkle.sol";
 import {FartherAirdrop1} from "../src/FartherAirdrop.sol";
 
 contract FartherAirdropTest is Test {
     FartherAirdrop1 public airdrop;
 
-    function setUp() public {
-        // airdrop = new FartherAirdrop1();
-    }
+    // address[] accounts = [
+    //     getFundedAccount(1),
+    //     getFundedAccount(2),
+    //     getFundedAccount(3)
+    // ];
+
+    // bytes32[] leaves;
+
+    // bytes32 public root;
+
+    // Merkle public m;
+
+    // function setUp() public {
+    //     // airdrop = new FartherAirdrop1();
+    // }
+
+    function test_deploy_succeeds() external {}
 }
