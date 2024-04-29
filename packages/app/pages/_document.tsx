57fcb57750750b8301335ff40eec832894688ae4
Matt Masurka <m.masurka@gmail.com>
2024-04-28 23:20:34 -0700

    favicons

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -6,6 +6,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1861cb" />
+      <meta name="msapplication-TileColor" content="#1861cb" />
+      <meta name="theme-color" content="#1861cb"></meta>
       <body
         className={cn(
           "min-h-screen overflow-x-hidden font-sans antialiased",
