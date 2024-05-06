5adc882e6c50e8bdd0eb0e71c9e8c44ec2c43847
Matt Masurka <m.masurka@gmail.com>
2024-05-06 12:28:22 -0700

    Adds phantom and safe wallets

--- packages/app/lib/walletConfig.ts
+++ packages/app/lib/walletConfig.ts
@@ -1,12 +1,38 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import {
+  coinbaseWallet,
+  metaMaskWallet,
+  phantomWallet,
+  safeWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http } from "wagmi";
 import { anvil, base, sepolia } from "wagmi/chains";
 
 export const WALLET_CONNECT_PROJECT_ID = "4861dc911064227b7cf8377990e49577";
 
-export const wagmiConfig = getDefaultConfig({
-  appName: "Farther",
-  projectId: WALLET_CONNECT_PROJECT_ID,
+export const wagmiConfig = createConfig({
+  connectors: connectorsForWallets(
+    [
+      {
+        groupName: "Recommended",
+        wallets: [
+          metaMaskWallet,
+          coinbaseWallet,
+          safeWallet,
+          phantomWallet,
+          walletConnectWallet,
+        ],
+      },
+    ],
+    { appName: "Farther", projectId: WALLET_CONNECT_PROJECT_ID },
+  ),
+  transports: {
+    [base.id]: http(process.env.BASE_RPC_URL),
+    [sepolia.id]: http(),
+    [anvil.id]: http(),
+  },
   chains: [base, sepolia, anvil],
   ssr: true,
 });
