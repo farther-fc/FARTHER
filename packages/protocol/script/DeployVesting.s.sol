cb969b0d0981a021b6cbbd9a286ebf74e9920d36
Matt Masurka <m.masurka@gmail.com>
2024-05-06 19:23:35 -0700

    FartherVesting

---
+++ packages/protocol/script/DeployVesting.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FartherVesting} from "../src/FartherVesting.sol";
+
+contract DeployVesting is Script {
+    function run() public {
+        vm.startBroadcast();
+
+        address OWNER = 0x97e3B75B2eebCC722B504851416e1410B32180a3;
+
+        uint64 START_TIME = 1746057600;
+        uint64 DURATION = 63072000; // 2 years in seconds
+
+        FartherVesting fartherVesting = new FartherVesting(
+            OWNER,
+            START_TIME,
+            DURATION
+        );
+
+        fartherVesting.transferOwnership(OWNER);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ packages/protocol/src/FartherVesting.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (finance/VestingWallet.sol)
+pragma solidity ^0.8.20;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {Address} from "openzeppelin/utils/Address.sol";
+import {Context} from "openzeppelin/utils/Context.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+
+import "forge-std/console.sol";
+
+/**
+ * @dev Forked from OpenZeppelin (finance/VestingWallet.sol)
+ * Adds a cliff to the vesting schedule.
+ */
+contract FartherVesting is Context, Ownable {
+    event EtherReleased(uint256 amount);
+    event ERC20Released(address indexed token, uint256 amount);
+
+    uint256 private _released;
+    mapping(address token => uint256) private _erc20Released;
+    uint64 private immutable _start;
+    uint64 private immutable _duration;
+    uint8 public constant CLIFF_PERCENTAGE = 25;
+
+    /**
+     * @dev Sets the sender as the initial owner, the beneficiary as the pending owner, the start timestamp and the
+     * vesting duration of the vesting wallet.
+     */
+    constructor(
+        address beneficiary,
+        uint64 startTimestamp,
+        uint64 durationSeconds
+    ) payable Ownable(beneficiary) {
+        _start = startTimestamp;
+        _duration = durationSeconds;
+    }
+
+    /**
+     * @dev The contract should be able to receive Eth.
+     */
+    receive() external payable virtual {}
+
+    /**
+     * @dev Getter for the start timestamp.
+     */
+    function start() public view virtual returns (uint256) {
+        return _start;
+    }
+
+    /**
+     * @dev Getter for the vesting duration.
+     */
+    function duration() public view virtual returns (uint256) {
+        return _duration;
+    }
+
+    /**
+     * @dev Getter for the end timestamp.
+     */
+    function end() public view virtual returns (uint256) {
+        return start() + duration();
+    }
+
+    /**
+     * @dev Amount of eth already released
+     */
+    function released() public view virtual returns (uint256) {
+        return _released;
+    }
+
+    /**
+     * @dev Amount of token already released
+     */
+    function released(address token) public view virtual returns (uint256) {
+        return _erc20Released[token];
+    }
+
+    /**
+     * @dev Getter for the amount of releasable eth.
+     */
+    function releasable() public view virtual returns (uint256) {
+        return vestedAmount(uint64(block.timestamp)) - released();
+    }
+
+    /**
+     * @dev Getter for the amount of releasable `token` tokens. `token` should be the address of an
+     * IERC20 contract.
+     */
+    function releasable(address token) public view virtual returns (uint256) {
+        return vestedAmount(token, uint64(block.timestamp)) - released(token);
+    }
+
+    /**
+     * @dev Release the native token (ether) that have already vested.
+     *
+     * Emits a {EtherReleased} event.
+     */
+    function release() public virtual {
+        uint256 amount = releasable();
+        _released += amount;
+        emit EtherReleased(amount);
+        Address.sendValue(payable(owner()), amount);
+    }
+
+    /**
+     * @dev Release the tokens that have already vested.
+     *
+     * Emits a {ERC20Released} event.
+     */
+    function release(address token) public virtual {
+        uint256 amount = releasable(token);
+        _erc20Released[token] += amount;
+        emit ERC20Released(token, amount);
+        SafeERC20.safeTransfer(IERC20(token), owner(), amount);
+    }
+
+    /**
+     * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(
+        uint64 timestamp
+    ) public view virtual returns (uint256) {
+        return _vestingSchedule(address(this).balance + released(), timestamp);
+    }
+
+    /**
+     * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(
+        address token,
+        uint64 timestamp
+    ) public view virtual returns (uint256) {
+        return
+            _vestingSchedule(
+                IERC20(token).balanceOf(address(this)) + released(token),
+                timestamp
+            );
+    }
+
+    /**
+     * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
+     * an asset given its total historical allocation.
+     */
+    function _vestingSchedule(
+        uint256 totalAllocation,
+        uint64 timestamp
+    ) internal view virtual returns (uint256) {
+        if (timestamp < start()) {
+            return 0;
+        } else if (timestamp >= end()) {
+            console.log("totalAllocation", totalAllocation);
+            return totalAllocation;
+        } else {
+            uint256 cliffAmount = (totalAllocation * CLIFF_PERCENTAGE) / 100;
+            uint256 remainingAmount = totalAllocation - cliffAmount;
+
+            console.log("cliffAmount", cliffAmount);
+            console.log("remainingAmount", remainingAmount);
+            return
+                cliffAmount +
+                ((remainingAmount * (timestamp - start())) / duration());
+        }
+    }
+}
