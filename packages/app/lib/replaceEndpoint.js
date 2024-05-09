d144edc6c4855b93df40d571a0aca30827560212
Matt Masurka <m.masurka@gmail.com>
2024-05-09 15:01:20 -0700

    ready to announce - still need to test though

--- packages/app/lib/replaceEndpoint.js
+++ packages/app/lib/replaceEndpoint.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
+// This is a janky way of getting around not knowing how to set an environment variable
+// for the endpoint in the GraphClient. It replaces the hardcoded endpoint with a reference.
+
 const graphClientPath = path.join(__dirname, "..", ".graphclient/index.ts");
 
 fs.readFile(graphClientPath, "utf8", (err, data) => {
--- packages/backend/src/scripts/prepareLPBonusDrop.ts
+++ packages/backend/src/scripts/prepareLPBonusDrop.ts
@@ -6,85 +6,18 @@ import {
   NETWORK,
   NEXT_AIRDROP_END_TIME,
   NEXT_AIRDROP_START_TIME,
-  UniswapV3StakerAbi,
-  WAD_SCALER,
-  contractAddresses,
   getMerkleRoot,
-  incentivePrograms,
-  tokenAllocations,
-  viemPublicClient,
 } from "@farther/common";
-import axios from "axios";
-import { formatEther, keccak256 } from "ethers";
 import { v4 as uuidv4 } from "uuid";
 import { AllocationType, prisma } from "../prisma";
+import { getLpAccounts } from "../utils/getLpAccounts";
 import { writeFile } from "../utils/helpers";
 import { airdropSanityCheck } from "./airdropSanityCheck";
-
-type Account = {
-  id: string;
-  rewardsClaimed: string;
-};
-
-const format = (n: string | bigint) => Number(formatEther(n)).toLocaleString();
-
-const incentiveRewardsTotalAllocation =
-  BigInt(tokenAllocations.liquidityRewards) * WAD_SCALER;
-const currentIncentiveProgramTotal =
-  incentiveRewardsTotalAllocation / BigInt(3);
 
 async function prepareLpBonusDrop() {
   await airdropSanityCheck();
 
-  // Get all liquidity providers who have claimed rewards
-  const query = await axios({
-    url: `https://farther.squids.live/farther-${ENVIRONMENT}/graphql`,
-    method: "post",
-    data: {
-      query: `
-      query LPRewardClaimers {
-        accounts(where: { rewardsClaimed_gt: 0 }) {
-          id
-          rewardsClaimed
-        }
-      }
-    `,
-    },
-  });
-
-  const accounts: Account[] = query.data.data.accounts;
-
-  for (const account of accounts) {
-    console.log(account.id, format(account.rewardsClaimed));
-  }
-
-  const totalActuallyClaimed = accounts.reduce(
-    (acc, a) => acc + BigInt(a.rewardsClaimed),
-    BigInt(0),
-  );
-
-  const [totalDrippedSoFar] = await viemPublicClient.readContract({
-    abi: UniswapV3StakerAbi,
-    address: contractAddresses.UNISWAP_V3_STAKER,
-    functionName: "incentives",
-    args: [
-      keccak256(
-        incentivePrograms[1].incentiveKey as `0x${string}`,
-      ) as `0x${string}`,
-    ],
-  });
-
-  const totalRemaining = currentIncentiveProgramTotal - totalDrippedSoFar;
-  const difference = totalActuallyClaimed - totalRemaining;
-
-  console.info({
-    incentiveRewardsTotalAllocation: format(incentiveRewardsTotalAllocation),
-    currentIncentiveProgramTotal: format(currentIncentiveProgramTotal),
-    totalDrippedSoFar: format(totalDrippedSoFar),
-    totalRemaining: format(totalRemaining),
-    totalActuallyClaimed: format(totalActuallyClaimed),
-    difference: format(difference),
-  });
+  const accounts = await getLpAccounts();
 
   // Get all past liquidity reward allocations
   const allocations = await prisma.allocation.findMany({
