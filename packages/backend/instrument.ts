4ba7dcb7bb2328064b507438b7bfeab6438b0be1
Matt Masurka <m.masurka@gmail.com>
2024-07-09 08:26:29 -0700

    Adds Sentry to backend (#40)
    * migrate:prod command fix
    
    * Adds Sentry to backend

---
+++ packages/backend/instrument.ts
@@ -0,0 +1,12 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://93c1dc8e6c8706cf1b8ed2bb911d57d4@o4506703122202624.ingest.us.sentry.io/4507572421853184",
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
