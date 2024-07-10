ac2a3f8bbfcd5c22760aaf39897dcee47ee74f4a
Matt Masurka <m.masurka@gmail.com>
2024-07-10 11:19:17 -0700

    Fixes getLatestCronTime.test.ts

--- packages/backend/src/lib/__tests__/getLatestCronTime.test.ts
+++ packages/backend/src/lib/__tests__/getLatestCronTime.test.ts
@@ -6,21 +6,27 @@ describe("getLatestCronTime", () => {
     mockDate("2024-07-08T05:05:00Z");
     const latestCronTime = getLatestCronTime(OPENRANK_SNAPSHOT_CRON);
     jest.useRealTimers();
-    expect(latestCronTime).toEqual(new Date("2024-07-08T03:00:00Z"));
+    expect(latestCronTime).toEqual(
+      new Date("2024-07-08T03:00:00Z").toISOString(),
+    );
   });
 
   test("returns 3pm of the same day if the current time is after 3pm", () => {
     mockDate("2024-07-08T20:00:00Z"); // 8pm UTC (example)
     const latestCronTime = getLatestCronTime(OPENRANK_SNAPSHOT_CRON);
     jest.useRealTimers();
-    expect(latestCronTime).toEqual(new Date("2024-07-08T15:00:00Z"));
+    expect(latestCronTime).toEqual(
+      new Date("2024-07-08T15:00:00Z").toISOString(),
+    );
   });
 
   test("returns 3pm of the previous day if the current time is before 3am", () => {
     mockDate("2024-07-08T01:00:00Z"); // 1am UTC (example)
     const latestCronTime = getLatestCronTime(OPENRANK_SNAPSHOT_CRON);
     jest.useRealTimers();
-    expect(latestCronTime).toEqual(new Date("2024-07-07T21:00:00Z"));
+    expect(latestCronTime).toEqual(
+      new Date("2024-07-07T21:00:00Z").toISOString(),
+    );
   });
 
   test("handles multiple cron hours correctly", () => {
@@ -28,12 +34,12 @@ describe("getLatestCronTime", () => {
     const cron = "0 2,4,6,8 * * *";
     const time1 = getLatestCronTime(cron);
     jest.useRealTimers();
-    expect(time1).toEqual(new Date("2024-07-08T06:00:00Z"));
+    expect(time1).toEqual(new Date("2024-07-08T06:00:00Z").toISOString());
 
     mockDate("2024-07-08T11:15:00Z");
     const time2 = getLatestCronTime(cron);
     jest.useRealTimers();
-    expect(time2).toEqual(new Date("2024-07-08T08:00:00Z"));
+    expect(time2).toEqual(new Date("2024-07-08T08:00:00Z").toISOString());
   });
 });
 
