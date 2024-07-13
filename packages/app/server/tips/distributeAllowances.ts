d6de201e7915d9c73120cb4375278aea88818e92
Matt Masurka <m.masurka@gmail.com>
2024-07-13 11:59:31 -0700

    maxDuration

--- packages/app/server/tips/distributeAllowances.ts
+++ packages/app/server/tips/distributeAllowances.ts
@@ -1,4 +1,6 @@
 import { adminProcedure } from "server/trpc";
 import { distributeAllowances as distribute } from "./utils/distributeAllowances";
 
+export const maxDuration = 300;
+
 export const distributeAllowances = adminProcedure.mutation(distribute);
--- packages/app/server/tips/updateEligibleTippers.ts
+++ packages/app/server/tips/updateEligibleTippers.ts
@@ -1,4 +1,6 @@
 import { adminProcedure } from "server/trpc";
 import { updateEligibleTippers as update } from "./utils/updateEligibleTippers";
 
+export const maxDuration = 300;
+
 export const updateEligibleTippers = adminProcedure.mutation(update);
