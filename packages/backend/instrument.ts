b5d959d671b3354a5c4e5dd3c9a4eab5df3902fb
Matt Masurka <m.masurka@gmail.com>
2024-07-12 19:20:19 -0700

    setting env in backend sentry

--- packages/backend/instrument.ts
+++ packages/backend/instrument.ts
@@ -1,7 +1,11 @@
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import { requireEnv } from "require-env-variable";
+
+const { NEXT_PUBLIC_ENVIRONMENT } = requireEnv("NEXT_PUBLIC_ENVIRONMENT");
 
 Sentry.init({
+  environment: NEXT_PUBLIC_ENVIRONMENT,
   dsn: "https://93c1dc8e6c8706cf1b8ed2bb911d57d4@o4506703122202624.ingest.us.sentry.io/4507572421853184",
   integrations: [nodeProfilingIntegration()],
   // Performance Monitoring
