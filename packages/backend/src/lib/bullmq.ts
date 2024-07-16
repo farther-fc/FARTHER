a33bf031275c58aa189d9820c0d7b4eecf1cec9f
Matt Masurka <m.masurka@gmail.com>
2024-07-16 14:06:40 -0700

    fixing connection string

--- packages/backend/src/lib/bullmq.ts
+++ packages/backend/src/lib/bullmq.ts
@@ -8,16 +8,20 @@ const { KV_REST_API_URL, KV_REST_API_TOKEN } = requireEnv(
   "KV_REST_API_TOKEN",
 );
 
-const host =
-  KV_REST_API_URL.includes("https://") || KV_REST_API_URL.split("https://")[1];
+const host = KV_REST_API_URL.includes("https://")
+  ? KV_REST_API_URL.split("https://")[1]
+  : KV_REST_API_URL.split("https://")[1];
 
-const getRedisConnection = () =>
-  new Redis(`redis://default:${KV_REST_API_TOKEN}@${host}:6379`, {
+const getRedisConnection = () => {
+  const connectionString = `redis://default:${KV_REST_API_TOKEN}@${host}:6379`;
+
+  return new Redis(connectionString, {
     maxRetriesPerRequest: null,
     tls: {
       rejectUnauthorized: false,
     },
   });
+};
 
 export const queueConnection =
   ENVIRONMENT === "development"
