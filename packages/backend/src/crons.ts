e553682d67d87caad52378f936d23b99a40762e2
Matt Masurka <m.masurka@gmail.com>
2024-07-17 15:36:39 -0700

    migrating to dotenvx

--- packages/backend/src/crons.ts
+++ packages/backend/src/crons.ts
@@ -1,26 +1,42 @@
 import { requireEnv } from "require-env-variable";
-import { generateApiCallCron } from "./lib/generateApiCallCron";
-import { takeOpenRankSnapshot } from "./lib/takeOpenRankSnapshot";
-import { updateTipperScores } from "./lib/updateTipperScores";
+// import { generateApiCallCron } from "./lib/generateApiCallCron";
+// import { takeOpenRankSnapshot } from "./lib/takeOpenRankSnapshot";
+// import { updateTipperScores } from "./lib/updateTipperScores";
 import { prisma } from "./prisma";
 
 const { CRON } = requireEnv("CRON");
 
 switch (CRON) {
   case "updateTipperScores": {
-    updateTipperScores().then(disconnect);
+    console.log(
+      "Running updateTipperScores on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    // updateTipperScores().then(disconnect);
     break;
   }
   case "takeOpenRankSnapshot": {
-    takeOpenRankSnapshot().then(disconnect);
+    console.log(
+      "Running takeOpenRankSnapshot on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    // takeOpenRankSnapshot().then(disconnect);
     break;
   }
   case "updateEligibleTippers": {
-    generateApiCallCron("admin.updateEligibleTippers")().then(disconnect);
+    console.log(
+      "Running updateEligibleTippers on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    // generateApiCallCron("admin.updateEligibleTippers")().then(disconnect);
     break;
   }
   case "distributeAllowances": {
-    generateApiCallCron("admin.distributeAllowances")().then(disconnect);
+    console.log(
+      "Running distributeAllowances on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    // generateApiCallCron("admin.distributeAllowances")().then(disconnect);
     break;
   }
   default: {
