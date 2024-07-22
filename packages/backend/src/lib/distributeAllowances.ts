47f1503e755fd453437db7a96b5f4044355d2bf4
Matt Masurka <m.masurka@gmail.com>
2024-07-22 10:11:05 -0700

    updates distributeAllowances

--- packages/backend/src/lib/distributeAllowances.ts
+++ packages/backend/src/lib/distributeAllowances.ts
@@ -1,30 +1,18 @@
 import {
+  DAILY_USD_TOTAL_ALLOWANCE,
   DistributeAllowancesError,
   ENVIRONMENT,
   cacheTypes,
   getHoursAgo,
   isProduction,
 } from "@farther/common";
-import { scaleLinear } from "d3";
 import { prisma } from "../prisma";
 import { getEligibleTippers, getExistingTippers } from "./getEligibleTippers";
-import { constrainWeights } from "./utils/constrainWeights";
-import { dailyTipDistribution } from "./utils/dailyTipDistribution";
 import { flushCacheType } from "./utils/flushCacheType";
 import { getPrice } from "./utils/getPrice";
-import { getTipMinimum } from "./utils/getTipMinimum";
-import { getUniqueTippees } from "./utils/getUniqueTippees";
-
-// Recovery threshold is a multiplier of the previous tip minimum.
-// When the tipper's allowance is at or below the recovery threshold, they receive a boost
-// to the weight determining their daily allowance relative to the total.
-const RECOVERY_THRESHOLD = 10;
-const MAX_RECOVERY_ADJUSTMENT_FACTOR = 25;
 
 export async function distributeAllowances() {
   const tipsMetas = await getTipMetas();
-
-  const currentDay = tipsMetas.length + 1;
 
   const previousMeta = tipsMetas[0];
 
@@ -39,7 +27,9 @@ export async function distributeAllowances() {
     }
   }
 
-  const defaultTotal = dailyTipDistribution({ currentDay });
+  const fartherUsdPrice = await getPrice();
+
+  const baseTotalAllowance = DAILY_USD_TOTAL_ALLOWANCE / fartherUsdPrice;
 
   // Get existing tippers to calculate the total unused allowance from the previous day
   const existingTippers = await getExistingTippers();
@@ -55,45 +45,17 @@ export async function distributeAllowances() {
     0,
   );
 
-  const availableTotalAllowance = defaultTotal + prevUnusedAllowance;
+  const availableTotalAllowance = baseTotalAllowance + prevUnusedAllowance;
 
   // Eligible tippers for new tip cycle
   const eligibleTippers = await getEligibleTippers();
 
-  const tipMinimum = Math.round(await getTipMinimum());
-
-  const weights = getWeights({
-    previousMeta,
-    eligibleTippers,
-    availableTotalAllowance,
-  });
-
-  const totalWeight = weights.reduce((sum, weight) => sum + weight, 0);
-
-  const combinedTipMinimums = tipMinimum * eligibleTippers.length;
-
-  if (combinedTipMinimums > availableTotalAllowance) {
-    throw new DistributeAllowancesError({
-      message: `Combined tip minimums exceeds total daily allowance`,
-    });
-  }
-
-  // Subtract tip min from total daily allowance (since min is automatically given to everyone)
-  const allowanceRemainder = availableTotalAllowance - combinedTipMinimums;
-
-  const fartherUsdPrice = await getPrice();
-
   if (process.env.NODE_ENV === "development" || ENVIRONMENT === "development") {
     printDevLogs({
-      currentDay,
       previousMeta,
       eligibleTippers,
       prevUnusedAllowance,
       availableTotalAllowance,
-      tipMinimum,
-      weights,
-      totalWeight,
-      allowanceRemainder,
       fartherUsdPrice,
     });
   }
@@ -101,23 +63,23 @@ export async function distributeAllowances() {
   await prisma.$transaction(async (tx) => {
     const tipMeta = await tx.tipMeta.create({
       data: {
-        tipMinimum,
+        tipMinimum: 0,
         totalAllowance: availableTotalAllowance,
         carriedOver: prevUnusedAllowance,
         usdPrice: fartherUsdPrice,
       },
     });
 
+    const allowancePerTipper = Math.floor(
+      availableTotalAllowance / eligibleTippers.length,
+    );
+
     // Distribute
     const newAllowances = eligibleTippers.map((tipper, i) => {
-      const amount = Math.floor(
-        tipMinimum + (weights[i] / totalWeight) * allowanceRemainder,
-      );
-
       return {
         userId: tipper.id,
         tipMetaId: tipMeta.id,
-        amount,
+        amount: allowancePerTipper,
         userBalance: tipper.totalBalance.toString(),
       };
     });
@@ -149,132 +111,17 @@ async function getTipMetas() {
   });
 }
 
-function getWeights({
-  previousMeta,
-  eligibleTippers,
-  availableTotalAllowance,
-}: {
-  previousMeta: Awaited<ReturnType<typeof getTipMetas>>[0];
-  eligibleTippers: Awaited<ReturnType<typeof getEligibleTippers>>;
-  availableTotalAllowance: number;
-}) {
-  const previousTipMin = previousMeta ? previousMeta.tipMinimum : 0;
-  const prevTotalAllowanceMinusTipMinimums = !previousMeta
-    ? 0
-    : previousMeta.totalAllowance -
-      previousTipMin * previousMeta.allowances.length;
-  const meanAllowance = previousMeta
-    ? prevTotalAllowanceMinusTipMinimums / previousMeta.allowances.length
-    : availableTotalAllowance / eligibleTippers.length;
-
-  const getRecoveryAdjustment = scaleLinear()
-    .domain([previousTipMin, previousTipMin * RECOVERY_THRESHOLD])
-    .range([MAX_RECOVERY_ADJUSTMENT_FACTOR, 0])
-    .clamp(true);
-
-  const weights = eligibleTippers.map((tipper) => {
-    let weight: number;
-
-    // New tippers don't have an allowance yet
-    if (!tipper.tipAllowances[0]) {
-      weight = previousMeta
-        ? (meanAllowance / prevTotalAllowanceMinusTipMinimums) * 0.75
-        : 1;
-    } else {
-      const prevAllowance = tipper.tipAllowances[0].amount;
-
-      const uniqueTippees = getUniqueTippees(tipper.tipsGiven);
-
-      const prevAllowanceMinusTipMinimum = prevAllowance - previousTipMin;
-      const recoveryAdjustment = getRecoveryAdjustment(prevAllowance);
-
-      const prevWeight =
-        prevAllowanceMinusTipMinimum / prevTotalAllowanceMinusTipMinimums;
-
-      const uniqueTippeeAdjustment = getUniqueTippeeAdjustment({
-        meanAllowance,
-        prevAllowance,
-        uniqueTippees,
-      });
-
-      weight =
-        uniqueTippees > 0
-          ? prevWeight * uniqueTippeeAdjustment * (1 + recoveryAdjustment)
-          : prevWeight;
-    }
-
-    return weight;
-  });
-
-  return constrainWeights({ weights, breadth: 0.6 });
-}
-
-const TIPPEE_ADJUSTMENT_FACTOR = 0.002;
-
-function getUniqueTippeeAdjustment({
-  meanAllowance,
-  prevAllowance,
-  uniqueTippees,
-}: {
-  meanAllowance: number;
-  prevAllowance: number;
-  uniqueTippees: number;
-}) {
-  // The unqiue tippee adjustment is dampened for tipper allowances that are higher than the mean allowance
-  const allowanceDampenerFn = scaleLinear()
-    .domain([meanAllowance, meanAllowance * 4])
-    .range([1, 0])
-    .clamp(true);
-  const allowanceDampener = allowanceDampenerFn(prevAllowance);
-
-  return uniqueTippees > 0
-    ? 1 + uniqueTippees * TIPPEE_ADJUSTMENT_FACTOR * allowanceDampener
-    : 1;
-}
-
-const TIPPER_IDS = {
-  acai: 246871,
-  wahoo: 3741,
-  kylepatrick: 247143,
-  bunglon: 502822,
-  matsuda: 334811,
-  marcelonada: 330083,
-  reneecampbell: 283144,
-};
-
-const TIPPER_NAMES = {
-  246871: "acai",
-  3741: "wahoo",
-  247143: "kylepatrick",
-  502822: "bunglon",
-  334811: "matsuda",
-  330083: "mercelonada",
-  283144: "reneecampbell",
-};
-
-type TipperNameKey = keyof typeof TIPPER_NAMES;
-
 function printDevLogs({
-  currentDay,
   previousMeta,
   eligibleTippers,
   prevUnusedAllowance,
   availableTotalAllowance,
-  tipMinimum,
-  weights,
-  totalWeight,
-  allowanceRemainder,
   fartherUsdPrice,
 }: {
-  currentDay: number;
   previousMeta: Awaited<ReturnType<typeof getTipMetas>>[0];
   eligibleTippers: Awaited<ReturnType<typeof getEligibleTippers>>;
   prevUnusedAllowance: number;
   availableTotalAllowance: number;
-  tipMinimum: number;
-  weights: number[];
-  totalWeight: number;
-  allowanceRemainder: number;
   fartherUsdPrice: number;
 }) {
   if (previousMeta) {
@@ -290,29 +137,7 @@ function printDevLogs({
   console.info(
     "Available total allowance:",
     availableTotalAllowance.toLocaleString(),
-    "Tip minimum:",
-    tipMinimum,
   );
 
   console.info(`Current price: $${fartherUsdPrice}`);
-
-  let allowanceTotal = 0;
-  // FOR TESTING PURPOSES
-  eligibleTippers.forEach((tipper, i) => {
-    const amount = Math.floor(
-      tipMinimum + (weights[i] / totalWeight) * allowanceRemainder,
-    );
-
-    allowanceTotal += amount;
-
-    if (Object.values(TIPPER_IDS).includes(tipper.id) || amount > 1000) {
-      console.log({
-        id: TIPPER_NAMES[tipper.id as TipperNameKey] || tipper.id,
-        amount,
-        // userBalance: tipper.totalBalance.toString(),
-      });
-    }
-  });
-
-  console.info("Total allowance:", allowanceTotal.toLocaleString());
 }
--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -274,3 +274,5 @@ export const WAD_SCALER = BigInt(10 ** 18);
 export const API_ENDPOINT_ROOT = `${ROOT_ENDPOINT}/api/v1` as const;
 
 export const BANNED_FIDS = [433756, 333165, 770214, 512251, 294499];
+
+export const DAILY_USD_TOTAL_ALLOWANCE = 3000;
