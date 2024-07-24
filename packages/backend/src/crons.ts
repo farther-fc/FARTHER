bf91a061014b811c93f6a37023265a628dafcc9c
Matt Masurka <m.masurka@gmail.com>
2024-07-24 08:01:01 -0700

    server command fix (#110)

--- packages/backend/src/crons.ts
+++ packages/backend/src/crons.ts
@@ -1,6 +1,11 @@
 import { requireEnv } from "require-env-variable";
 import { distributeAllowances } from "./lib/distributeAllowances";
+import { syncTipperData } from "./lib/syncTipperData";
 import { syncUserData } from "./lib/syncUserData";
+import { takeOpenRankSnapshot } from "./lib/takeOpenRankSnapshot";
+import { updateEligibleTippers } from "./lib/updateEligibleTippers";
+import { updateTipperScores } from "./lib/updateTipperScores";
+import { prisma } from "./prisma";
 
 const { CRON } = requireEnv("CRON");
 
@@ -8,51 +13,58 @@ switch (CRON) {
   case "syncUserData": {
     console.log("Running syncUserData on", process.env.NEXT_PUBLIC_ENVIRONMENT);
 
+    // Uses worker - disconnect is called in the worker
     syncUserData();
     break;
   }
-  // case "syncTipperData": {
-  //   console.log(
-  //     "Running syncTipperData on",
-  //     process.env.NEXT_PUBLIC_ENVIRONMENT,
-  //   );
+  case "syncTipperData": {
+    console.log(
+      "Running syncTipperData on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
 
-  //   syncTipperData();
-  //   break;
-  // }
-  // case "updateTipperScores": {
-  //   console.log(
-  //     "Running updateTipperScores on",
-  //     process.env.NEXT_PUBLIC_ENVIRONMENT,
-  //   );
-  //   updateTipperScores();
-  //   break;
-  // }
-  // case "takeOpenRankSnapshot": {
-  //   console.log(
-  //     "Running takeOpenRankSnapshot on",
-  //     process.env.NEXT_PUBLIC_ENVIRONMENT,
-  //   );
-  //   takeOpenRankSnapshot();
-  //   break;
-  // }
-  // case "updateEligibleTippers": {
-  //   console.log(
-  //     "Running updateEligibleTippers on",
-  //     process.env.NEXT_PUBLIC_ENVIRONMENT,
-  //   );
-  //   updateEligibleTippers();
-  //   break;
-  // }
+    // Uses worker - disconnect is called in the worker
+    syncTipperData();
+    break;
+  }
+  case "updateTipperScores": {
+    console.log(
+      "Running updateTipperScores on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    updateTipperScores().then(disconnectCron);
+    break;
+  }
+  case "takeOpenRankSnapshot": {
+    console.log(
+      "Running takeOpenRankSnapshot on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    takeOpenRankSnapshot().then(disconnectCron);
+    break;
+  }
+  case "updateEligibleTippers": {
+    console.log(
+      "Running updateEligibleTippers on",
+      process.env.NEXT_PUBLIC_ENVIRONMENT,
+    );
+    updateEligibleTippers().then(disconnectCron);
+    break;
+  }
   case "distributeAllowances": {
     console.log(
       "Running distributeAllowances on",
       process.env.NEXT_PUBLIC_ENVIRONMENT,
     );
-    distributeAllowances();
+    distributeAllowances().then(disconnectCron);
     break;
   }
   default: {
     console.error("Unknown cron");
   }
+}
+
+export async function disconnectCron() {
+  prisma.$disconnect();
+  process.exit(0);
 }
