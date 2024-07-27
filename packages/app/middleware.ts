2c5a647799667bc37d904ee761a4304b032f2388
Matt Masurka <m.masurka@gmail.com>
2024-07-27 11:45:17 -0700

    increase API rate limit for external traffic (#131)

--- packages/app/middleware.ts
+++ packages/app/middleware.ts
@@ -2,21 +2,37 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
-const ratelimit = new Ratelimit({
+const rateLimitWebsite = new Ratelimit({
   redis: kv,
-  // 5 requests from the same IP in 10 seconds
-  limiter: Ratelimit.slidingWindow(7, "10 s"),
+  limiter: Ratelimit.slidingWindow(20, "10 s"),
 });
 
-// Define which routes you want to rate limit
+const rateLimitDefault = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(2, "10 s"),
+});
+
 export const config = {
   matcher: "/api/:path*",
 };
 
 export default async function middleware(request: NextRequest) {
-  // You could alternatively limit based on user ID or similar
+  // Determine the source of the request
+  const referer = request.headers.get("referer");
   const ip = request.ip ?? "127.0.0.1";
-  const { success, limit, reset, remaining } = await ratelimit.limit(ip);
+
+  let success, limit, reset, remaining;
+
+  if (
+    referer &&
+    (referer.includes("farther.social") || referer.includes("localhost:3000"))
+  ) {
+    // Apply website-specific rate limiting
+    ({ success, limit, reset, remaining } = await rateLimitWebsite.limit(ip));
+  } else {
+    // Apply default rate limiting for other sources
+    ({ success, limit, reset, remaining } = await rateLimitDefault.limit(ip));
+  }
 
   return success
     ? NextResponse.next()
