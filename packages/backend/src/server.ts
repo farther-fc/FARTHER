8b9e9cc60bf0eedf8a9f91e2d4faa6e229f0805e
Matt Masurka <m.masurka@gmail.com>
2024-07-29 09:25:20 -0700

    Removes test (#144)

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -54,7 +54,3 @@ cron.schedule(
 cron.schedule(cronSchedules.UPDATE_TIPPER_SCORES, createTipperScores, {
   timezone: "Etc/UTC",
 });
-
-setTimeout(() => {
-  throw new Error("testing error to see if it shows up in railway logs");
-}, 60_000);
