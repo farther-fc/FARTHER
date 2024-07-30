44f960fafb6e13f831afab4ed1c2f64afb2ab08b
Matt Masurka <m.masurka@gmail.com>
2024-07-29 20:31:40 -0700

    openRankScore.scoreNew (decimal)

---
+++ packages/backend/src/prisma/migrations/20240730032028_openrank_score_score_new/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "OpenRankScore" ADD COLUMN     "scoreNew" DECIMAL(65,30);
---
+++ packages/backend/src/scripts/testOpenRankDecimal.ts
@@ -0,0 +1,40 @@
+import { OPENRANK_URL, axios } from "@farther/common";
+import Decimal from "decimal.js";
+import { prisma } from "../prisma";
+
+async function testOpenRankDecimal() {
+  const response = await axios.post(OPENRANK_URL, [4378]);
+
+  const score = response.data.result[0].score;
+
+  const openRankScore = await prisma.openRankScore.create({
+    data: {
+      snapshot: {
+        connectOrCreate: {
+          where: {
+            id: "1",
+          },
+          create: {
+            id: "1",
+          },
+        },
+      },
+      user: {
+        connectOrCreate: {
+          where: {
+            id: 4378,
+          },
+          create: {
+            id: 4378,
+          },
+        },
+      },
+      score: score,
+      scoreNew: new Decimal(score),
+    },
+  });
+
+  console.log(score, openRankScore);
+}
+
+testOpenRankDecimal();
