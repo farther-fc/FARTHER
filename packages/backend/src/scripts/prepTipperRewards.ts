32e0ababcfdb4488808900693cb571ce5d1de274
Matt Masurka <m.masurka@gmail.com>
2024-08-26 16:12:14 -0700

    August tipper rewards

--- packages/backend/src/scripts/prepTipperRewards.ts
+++ packages/backend/src/scripts/prepTipperRewards.ts
@@ -1,17 +1,27 @@
 import {
   CHAIN_ID,
+  ENVIRONMENT,
   NEXT_AIRDROP_END_TIME,
   NEXT_AIRDROP_START_TIME,
   TIPPER_REWARDS_POOL,
   getMerkleRoot,
   neynar,
 } from "@farther/common";
+import { writeFileSync } from "fs";
 import { Address } from "viem";
-import { prisma } from "../prisma";
+import { createAllocationsCSV } from "../lib/createAllocationsCSV";
+import { AllocationType, prisma } from "../prisma";
+import { airdropSanityCheck } from "./airdropSanityCheck";
 
-const currentPrice = 0.0025;
+import { v4 as uuidv4 } from "uuid";
 
 async function prepTipperRewards() {
+  await airdropSanityCheck({
+    date: NEXT_AIRDROP_START_TIME,
+    network: "base",
+    environment: ENVIRONMENT,
+  });
+
   const latestSnapshot = await prisma.tipperScoreSnapshot.findFirst({
     orderBy: {
       createdAt: "desc",
@@ -52,25 +62,31 @@ async function prepTipperRewards() {
     return acc + tipper.tipperScores[0].score;
   }, 0);
 
-  const totalRewardsPool = TIPPER_REWARDS_POOL * currentPrice;
+  const allocations = tippers
+    .map((tipper) => {
+      const user = userData.find((user) => user.fid === tipper.id);
 
-  const leafs = tippers.map((tipper, index) => {
-    const user = userData.find((user) => user.fid === tipper.id);
+      if (!user) {
+        throw new Error(`No user found for fid: ${tipper.id}`);
+      }
 
-    if (!user) {
-      throw new Error(`No user found for fid: ${tipper.id}`);
-    }
+      const score = tipper.tipperScores[0].score;
+      const weight = score / totalScore;
+      return {
+        address: user.verified_addresses.eth_addresses[0] as Address,
+        fid: user.fid,
+        username: user.username,
+        amount: weight * TIPPER_REWARDS_POOL,
+      };
+    })
+    .sort((a, b) => b.amount - a.amount)
+    .map((a, index) => ({ ...a, index }));
 
-    const score = tipper.tipperScores[0].score;
-    const weight = score / totalScore;
-    return {
-      address: user.verified_addresses.eth_addresses[0] as Address,
-      index,
-      amount: BigInt(
-        Math.round(weight * totalRewardsPool * 10 ** 18),
-      ).toString(),
-    };
-  });
+  const leafs = allocations.map((a) => ({
+    address: a.address,
+    index: a.index,
+    amount: getBigIntStringAmount(a.amount),
+  }));
 
   const allocationSum = leafs.reduce(
     (acc, leaf) => BigInt(acc) + BigInt(leaf.amount),
@@ -90,13 +106,42 @@ async function prepTipperRewards() {
       },
     });
 
-    // await tx.allocation.create({
-    //   data: {
-    //     totalRewardsPool,
-    //     snapshotId: latestSnapshot.id,
-    //   },
-    // });
+    const createManyResponse = await tx.allocation.createMany({
+      data: allocations.map((a, i) => ({
+        id: uuidv4(),
+        userId: a.fid,
+        index: a.index,
+        airdropId: airdrop.id,
+        type: AllocationType.TIPPER,
+        address: a.address.toLowerCase(),
+        amount: getBigIntStringAmount(a.amount),
+      })),
+    });
+
+    console.log(`Created ${createManyResponse.count} allocations`);
   });
+
+  await writeFileSync(
+    `airdrops/${ENVIRONMENT}/${AllocationType.TIPPER.toLowerCase()}-${NEXT_AIRDROP_START_TIME.toISOString()}.json`,
+    JSON.stringify(
+      {
+        root,
+        amount: allocationSum.toString(),
+        leafs,
+      },
+      null,
+      2,
+    ),
+  );
+
+  await createAllocationsCSV({
+    allocationType: AllocationType.TIPPER,
+    leafs,
+  });
+}
+
+function getBigIntStringAmount(amount: number) {
+  return BigInt(Math.round(amount * 10 ** 18)).toString();
 }
 
 prepTipperRewards();
