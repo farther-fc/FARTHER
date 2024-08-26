21c57b6c32bf6f4c40c5a7bcf402ff5633bfea5e
Matt Masurka <m.masurka@gmail.com>
2024-08-26 15:33:32 -0700

    disable server (#252)
    * august lp bonus rewards & tips
    
    * Removes pending lp bonus rewards from profile page
    
    * Disable server

--- packages/backend/src/scripts/prepTipperRewards.ts
+++ packages/backend/src/scripts/prepTipperRewards.ts
@@ -1,68 +1,102 @@
-import { TIPPER_REWARDS_POOL, getStartOfMonthUTC } from "@farther/common";
+import {
+  CHAIN_ID,
+  NEXT_AIRDROP_END_TIME,
+  NEXT_AIRDROP_START_TIME,
+  TIPPER_REWARDS_POOL,
+  getMerkleRoot,
+  neynar,
+} from "@farther/common";
+import { Address } from "viem";
 import { prisma } from "../prisma";
 
 const currentPrice = 0.0025;
 
 async function prepTipperRewards() {
+  const latestSnapshot = await prisma.tipperScoreSnapshot.findFirst({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!latestSnapshot) {
+    throw new Error("No latest snapshot found");
+  }
+
   const tippers = await prisma.user.findMany({
     where: {
-      AND: [
-        {
-          tipsGiven: {
-            some: {
-              createdAt: {
-                gte: getStartOfMonthUTC(0),
-              },
-            },
+      tipperScores: {
+        some: {
+          snapshotId: latestSnapshot.id,
+          score: {
+            gt: 0,
           },
         },
-        {
-          tipperScores: {
-            some: {
-              score: {
-                gt: 0,
-              },
-            },
-          },
-        },
-      ],
+      },
     },
     select: {
       id: true,
       tipperScores: {
-        orderBy: {
-          createdAt: "desc",
-        },
-        take: 1,
-        select: {
-          score: true,
+        where: {
+          snapshotId: latestSnapshot.id,
+          score: {
+            gt: 0,
+          },
         },
       },
     },
   });
 
+  const userData = await neynar.getUsersByFid(tippers.map((u) => u.id));
+
   const totalScore = tippers.reduce((acc, tipper) => {
     return acc + tipper.tipperScores[0].score;
   }, 0);
 
-  const rewards = tippers.map((tipper) => {
+  const totalRewardsPool = TIPPER_REWARDS_POOL * currentPrice;
+
+  const leafs = tippers.map((tipper, index) => {
+    const user = userData.find((user) => user.fid === tipper.id);
+
+    if (!user) {
+      throw new Error(`No user found for fid: ${tipper.id}`);
+    }
+
     const score = tipper.tipperScores[0].score;
     const weight = score / totalScore;
     return {
-      userId: tipper.id,
-      score: score,
-      reward: `$${(weight * TIPPER_REWARDS_POOL * currentPrice).toLocaleString()}`,
+      address: user.verified_addresses.eth_addresses[0] as Address,
+      index,
+      amount: BigInt(
+        Math.round(weight * totalRewardsPool * 10 ** 18),
+      ).toString(),
     };
   });
 
-  console.log(rewards.sort((a, b) => b.score - a.score));
-
-  const medianReward = rewards[Math.floor(rewards.length / 2)].reward;
-
-  console.log(
-    `averageReward: ${(TIPPER_REWARDS_POOL * currentPrice) / rewards.length}`,
+  const allocationSum = leafs.reduce(
+    (acc, leaf) => BigInt(acc) + BigInt(leaf.amount),
+    BigInt(0),
   );
-  console.log(`medianReward: ${medianReward}`);
+
+  const root = getMerkleRoot(leafs);
+
+  await prisma.$transaction(async (tx) => {
+    const airdrop = await prisma.airdrop.create({
+      data: {
+        chainId: CHAIN_ID,
+        amount: allocationSum.toString(),
+        root,
+        startTime: NEXT_AIRDROP_START_TIME,
+        endTime: NEXT_AIRDROP_END_TIME,
+      },
+    });
+
+    // await tx.allocation.create({
+    //   data: {
+    //     totalRewardsPool,
+    //     snapshotId: latestSnapshot.id,
+    //   },
+    // });
+  });
 }
 
 prepTipperRewards();
--- packages/backend/src/scripts/temp.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from "fs";
-
-async function temp() {
-  const jsonData = await readFileSync("../../airdrops/");
-}
-
-temp();
